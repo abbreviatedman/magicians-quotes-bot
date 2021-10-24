6f89528c5568056a4caed1f7af269d491cabc740
Colin Jaffe <balloonasaurus@gmail.com>
2021-10-24 11:45:22 -0400

    Refactors main algorithm.

--- main.js
+++ main.js
@@ -14,21 +14,13 @@ const quotes = [
 ];
 
 const unusedIds = {};
-quotes.forEach((quote) => (unusedIds[quote.id] = true));
-
 setInterval(() => {
-  const ids = Object.keys(unusedIds);
-  let found = false;
-  while (!found) {
-    const randomId = ids[Math.floor(Math.random() * ids.length)];
-    if (randomId in unusedIds) {
-      found = true;
-      delete unusedIds[randomId];
-      console.log(quotes[randomId - 1].text);
-    }
-  }
-
   if (!Object.keys(unusedIds).length) {
     quotes.forEach((quote) => (unusedIds[quote.id] = true));
   }
+
+  const ids = Object.keys(unusedIds);
+  const randomId = ids[Math.floor(Math.random() * ids.length)];
+  console.log(quotes.find((quote) => quote.id == randomId).text);
+  delete unusedIds[randomId];
 }, 2000);
